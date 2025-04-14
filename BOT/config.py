85c33ebfebdf5872c7203d2e509404941b78ed21
Reo <84664529+reo-s@users.noreply.github.com>
2025-04-14 23:19:34 +0500

    Update config.py

--- BOT/config.py
+++ BOT/config.py
@@ -1,2 +1,7 @@
-API_TOKEN="ваш-токен"
-ADMIN_ID=1234567890 # Ваш ID (@userinfobot)
+# API_TOKEN="ваш-токен"
+# ADMIN_ID=1234567890 # Ваш ID (@userinfobot)
+
+import os
+
+API_TOKEN = os.getenv("API_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
